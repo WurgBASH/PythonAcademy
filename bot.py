b6af48650798a428b32bee8ca27a60cd359f3e92
WurgBASH <just_my_mail@coolsite.net>
2019-05-14 23:52:57 +0300

    ver 0.1.4 fixed

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ dispatcher = updater.dispatcher
 l1_cqd = 'L1BUT'
 
 #lesson1_button = telegram.InlineKeyboardButton(text='Як встановити Python?',callback_data=l1_cqd)
-lesson1_button = telegram.InlineKeyboardButton(text='Як встановити Python?',link="https://telegra.ph/YAk-vstanoviti-Python-05-13")
+lesson1_button = telegram.InlineKeyboardButton(text='Як встановити Python?',url="https://telegra.ph/YAk-vstanoviti-Python-05-13")
 
 def main_menu(bot, update):
 	kb = [[telegram.KeyboardButton('Список уроків')],
